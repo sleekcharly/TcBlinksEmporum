422b53f1b3c424f72784048eebf9f18b0b0e49a1
Charles <c_ukasoanya@yahoo.com>
2024-01-23 20:28:00 +0100

    setup middleware file

---
+++ middleware.ts
@@ -0,0 +1,62 @@
+import NextAuth from 'next-auth';
+
+import { auth } from './auth';
+
+// routes
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  apiAuthPrefix,
+  authRoutes,
+  publicRoutes,
+} from './routes';
+
+export default auth((req) => {
+  const { nextUrl } = req;
+
+  //  check if user is logged in
+  const isLoggedIn = !!req.auth;
+
+  // check if user visited the auth api route
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+
+  //check if user visited the public route
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+
+  // check if user visited the auth route
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) {
+    return null;
+  }
+
+  // redirect user to default redirect if user is logged on
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+
+    return null;
+  }
+
+  // return user to last visited page before logging in
+  if (!isLoggedIn && !isPublicRoute) {
+    let callbackUrl = nextUrl.pathname;
+    if (nextUrl.search) {
+      callbackUrl += nextUrl.search;
+    }
+
+    // encode callback url
+    const encodedCallbackUrl = encodeURIComponent(callbackUrl);
+
+    return Response.redirect(
+      new URL(`/auth/login?callbackUrl=${encodedCallbackUrl}`, nextUrl),
+    );
+  }
+
+  return null;
+});
+
+// Optionally, don't invoke Middleware on some paths
+export const config = {
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
